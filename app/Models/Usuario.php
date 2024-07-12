4a671e5513b4b47fb16b82d10e3d26a9792bf5a4
Mateus Pereira <mhapereira99@gmail.com>
2024-07-12 11:38:24 -0300

    Ajuste FilamentUser

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Usuario extends Authenticatable
+class Usuario extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasVerifiedEmail();
+    }
 
     // Especifica o nome da tabela personalizada
     protected $table = 'usuario';
