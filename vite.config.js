95fc108f82913e1ec0b494993227b9d8d22eac91
Mateus Pereira <mhapereira99@gmail.com>
2024-07-15 11:29:56 -0300

    /build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
+    build: {
+      outDir: path.resolve(__dirname, 'public/build'),
+      manifest: true,
+  },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
